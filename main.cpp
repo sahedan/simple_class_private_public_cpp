3d7409502e5713038d0810daabdc2cf89207e26a
sahedan <edansalam@gmail.com>
2017-09-11 23:07:52 +0800

    simple private and public

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+class Box
+{
+public:
+double length; // Length of a box
+double breadth; // Breadth of a box
+double height; // Height of a box
+};
+
+
+
+int main( )
+{
+Box Box1; // Declare Box1 of type Box
+Box Box2; // Declare Box2 of type Box
+double volume = 0.0; // Store the volume of a box here
+
+
+// box 1 specification
+Box1.height = 4.0;
+Box1.length = 6.0;
+Box1.breadth = 3.0;
+
+// box 2 specification
+Box2.height = 10.0;
+Box2.length = 12.0;
+Box2.breadth = 12.0;
+
+// volume of box 1
+volume = Box1.height * Box1.length * Box1.breadth;
+cout << "Volume of Box1 : " << volume <<endl;
+
+// volume of box 2
+volume = Box2.height * Box2.length * Box2.breadth;
+cout << "Volume of Box2 : " << volume <<endl;
+
+
+class Bottle1
+{
+private:
+double length = 9.0; // Length of a box
+double breadth = 9.0; // Breadth of a box
+double height = 9.0; // Height of a box
+//double volume; // Store the volume of a box here
+
+// volume of box 1
+double volume = height * length * breadth;
+
+};
+cout << "Volume of Bottle1 : " << volume <<endl;
+return 0;
+}
